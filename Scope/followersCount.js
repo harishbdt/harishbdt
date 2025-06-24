6bacb7c5e70e60651bce6c4ef760b919b461aa63
Harish Y <harishbdt@gmail.com>
2025-06-24 09:50:51 -0400

    follower count

---
+++ Scope/followersCount.js
@@ -0,0 +1,19 @@
+let count = 0; // Initialize count to 0
+
+function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count; //Display the count in the HTML
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your followers count reached to 10. Congratulations");
+    } else if (count === 20) {
+        alert("Your followers count reached to 20. Congratulations");
+    }
+}
